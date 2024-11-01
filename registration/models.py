971ac93369528f7603842844e734195c032a2e65
17MrRobot71 <bosh0705@gmail.com>
2024-11-01 12:03:47 +0200

    Registration

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+
+
+class Customer(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def str(self):
+        return self.email
+
