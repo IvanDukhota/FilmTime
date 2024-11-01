3a3e1025d6b171bb6addca7dff1bc3857e3af143
IvanDukhota <ivan.dukhota@gmail.com>
2024-11-01 13:24:12 +0200

    Authorization

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,11 @@
 # registration/urls.py
 from django.urls import path
-from .views import CustomerListView, CustomerDetailView, CheckUserView
+from .views import CustomerListView, CustomerDetailView, CheckUserView, RegisterUserView, LoginView
 
 urlpatterns = [
-    path('registration-users/', CustomerListView.as_view(), name='userishka-list'),  # Список пользователей
-    path('registration-users/<int:id>/', CustomerDetailView.as_view(), name='userishka-detail'),  # Один пользователь
+    path('registration-users/', CustomerListView.as_view(), name='userishka-list'),  
+    path('registration-users/<int:id>/', CustomerDetailView.as_view(), name='userishka-detail'), 
     path('check-user/', CheckUserView.as_view(), name='check-user'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
