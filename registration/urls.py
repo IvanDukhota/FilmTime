a919023b7e0025ef23600caa612afd8007aad5ec
17MrRobot71 <bosh0705@gmail.com>
2024-11-03 20:51:59 +0200

    Facebook Api Auth

--- registration/urls.py
+++ registration/urls.py
@@ -1,6 +1,6 @@
 # registration/urls.py
 from django.urls import path
-from .views import UserListView, UserDetailView, CheckUserView, RegisterUserView, LoginView,  google_login,google_callback
+from .views import UserListView, UserDetailView, CheckUserView, RegisterUserView, LoginView,  google_login,google_callback, facebook_login, facebook_callback
 
 urlpatterns = [
     path('registration-users/', UserListView.as_view(), name='userishka-list'),  
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('google/login/', google_login, name='google_login'),
     path('google/callback/', google_callback, name='google_callback'),
+    path('facebook/login/', facebook_login, name='facebook-login'),
+    path('facebook/callback/', facebook_callback, name='facebook-callback'),
 ]
