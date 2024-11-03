f856100fec4723ff48ace0f612219aecaac9bb3a
17MrRobot71 <bosh0705@gmail.com>
2024-11-03 19:00:17 +0200

    google api auth

--- FilmTime/settings.py
+++ FilmTime/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = "FilmTime.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'FilmTime',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'FilmTime',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
