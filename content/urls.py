aee43205effbd81654189353b5783e1190896936
17MrRobot71 <bosh0705@gmail.com>
2024-12-07 20:34:02 +0200

    Created ability to CRUD own Content List

--- content/urls.py
+++ content/urls.py
@@ -1,11 +1,14 @@
 # registration/urls.py
 from django.urls import path
-from .views import Movies, MovieDetailView, ContentViewSet
+from .views import Movies, MovieDetailView, ContentViewSet, RecordMovieView, ContentListView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('movie/', Movies.movie_link, name='movie_link'),
     path('movie/<int:content_id>/', MovieDetailView.as_view(), name='movie-detail'),
     path('movies/filter/', ContentViewSet.as_view({'get': 'filter_movies'}), name='movie-filter'),
+    path("movie/<int:content_id>/record-view/", RecordMovieView.as_view(), name="record_movie_view"),
+
+    path('content-list/', ContentListView.as_view(), name='content-list'),
 ]
 
